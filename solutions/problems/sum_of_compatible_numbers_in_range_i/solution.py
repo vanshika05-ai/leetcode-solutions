30303476016f2d6e1c230300c4488a2b7c892499
Vanshika <pundirvanshika9@gmail.com>
2026-06-07 03:55:15 +0000

    Sync LeetCode submission - Sum of Compatible Numbers in Range I (python3)

---
+++ solutions/problems/sum_of_compatible_numbers_in_range_i/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def sumOfGoodIntegers(self, n: int, k: int) -> int:
+        total  = 0
+        for x in range(max(1,n-k),n+k+1):
+            if(n&x) == 0:
+                total += x
+
+        return total
+        
